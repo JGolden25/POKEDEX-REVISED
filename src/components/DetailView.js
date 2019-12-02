d322e2c74832156f89656771400635c2e7c18142
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-02 14:31:23 -0600

    pokemon decontructed within detail view

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles/DetailView.css';
 
-const DetailView = () => {
-
+const DetailView = ({ pokemon }) => {
+    const { id, name, sprite, type } = pokemon;
     return (
       <section className="detail-view">
-        <img className='sprite-image' />
+        <img src={sprite} className='sprite-image' alt="sprite"/>
         <div className='data-wrapper'>
-          <h1 className='data-name'></h1>
-          <p className="data-char"></p>
+          <h1 className='data-name'>ID: {id} {name}</h1>
+          <p className="data-char">Type: {type}</p>
         </div>
       </section>
     )
