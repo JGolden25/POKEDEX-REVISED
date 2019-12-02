1018c9ab30b4a957aadbff3b30ab5bd2747ba5e0
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-02 14:20:08 -0600

    state introduced via pokemon

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
 import DetailView from './DetailView';
+import Pokemon from '../Pokemon';
 import './styles/App.css';
 
 
@@ -8,17 +9,26 @@ import './styles/App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      pokemon: {}
+    };
     this.handleOnClick = this.handleOnClick.bind(this);
   }
   handleOnClick(id) {
-    console.log(id);
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+        this.setState({ pokemon });
+        //console.log(pokemon);
+      })
+      .catch(err => console.log(err));
   }
   render() {
     return (
       <div className="App">
         <PokeList handleOnClick={this.handleOnClick}/>
-        <DetailView />
+        <DetailView pokemon={this.state.pokemon}/>
       </div>
     );
   }
