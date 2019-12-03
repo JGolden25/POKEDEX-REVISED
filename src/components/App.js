9217091f1cee2233db45ffbb0971e7fbd5ec210d
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-03 14:30:02 -0600

    select detail fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList';
+import Pokemon from '../Pokemon';
 import DetailView from './DetailView';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
+
 
 
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {pokemon: {}};
     this.handleOnClick = this.handleOnClick.bind(this);
   }
   handleOnClick(id) {
+    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+      .then(res => res.json())
+      .then(data => {
+        const pokemon = new Pokemon(data);
+        this.setState({ pokemon });
+        console.log(pokemon);
+      })
+      .catch(err => console.log(err));
     console.log(id);
   }
   render() {
     return (
       <div className="App">
         <PokeList handleOnClick={this.handleOnClick}/>
-        <DetailView />
+        <DetailView pokemon={this.state.pokemon}/>
       </div>
     );
   }
--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './styles/DetailView.css';
 
-const DetailView = () => {
+const DetailView = ({ pokemon }) => {
+  const { id, name, sprite, type } = pokemon;
 
-    return (
-      <section className="detail-view">
-        <img className='sprite-image' />
-        <div className='data-wrapper'>
-          <h1 className='data-name'></h1>
-          <p className="data-char"></p>
-        </div>
-      </section>
-    )
-  }
-  
-  export default DetailView;
+  return (
+    <section className="detail-view">
+      <img src={sprite} className='sprite-image' alt="sprite"/>
+      <div className='data-wrapper'>
+        <h1 className='data-name'>ID: {id} {name}</h1>
+        <p className="data-char">Type: {type}</p>
+      </div>
+    </section>
+  )
+}
+
+export default DetailView;
