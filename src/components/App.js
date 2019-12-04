9ba62cd2f5c63bc53b758a0ee308f090ba03c535
Joseph Chretien-Golden <joechre@gmail.com>
2019-12-03 18:42:36 -0600

    herokusetup

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     this.handleOnClick = this.handleOnClick.bind(this);
   }
   handleOnClick(id) {
-    fetch(`http://pokeapi.co/api/v2/pokemon/${id}/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
       .then(res => res.json())
       .then(data => {
         const pokemon = new Pokemon(data);
